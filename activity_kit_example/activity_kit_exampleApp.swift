d733f809b38f02d4724244066bfeebc4bfe0db35
Saransh Singhal <singhalsaransh40@gmail.com>
2024-07-27 19:01:27 +0530

    Initial Commit

---
+++ activity_kit_example/activity_kit_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  activity_kit_exampleApp.swift
+//  activity_kit_example
+//
+//  Created by Saransh Singhal on 27/07/24.
+//
+
+import SwiftUI
+
+@main
+struct activity_kit_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
